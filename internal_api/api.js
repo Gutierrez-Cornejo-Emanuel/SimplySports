6f1e72bc866d0505c8f93b44181f4bb17f00ccf8
Emanuel <emanuelgtz02@gmail.com>
2023-02-15 17:57:32 -0800

    added hashing to passwords, should be ready to test

--- internal_api/api.js
+++ internal_api/api.js
@@ -1,26 +1,41 @@
+/*
+Dependencies:
+    express
+    mongodb
+    bcrypt
+    dotenv
+    body-parser
+*/
+
 const { Router } = require("express");
 const { MongoClient, ObjectId } = require("mongodb");
 
-
+const bcrypt = require("bcrypt");
 
 
 if (process.env.MODE === "production" ) {
-    url = `remote_url`; 
+    url = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_URL}/`; 
 }
 else {
     url = "mongodb://127.0.0.1:27017/";
 }
 const client = new MongoClient(url);
 const db = client.db(
-    process.env.MODE === "production" ? "SimplySports": "sports"
+    process.env.MODE === "production" ? "team19": "sports"
 );
 const apiRouter = Router();
 
 
+//Endpoint to test that api is running, to be removed
+apiRouter.get("/", (req,res) => {
+    res.send("This is a test");
+});
+
 apiRouter.post("/signup/", async (req, res) => {
     try {
         const users = db.collection("users");
-        const user_info = {username: req.body.username, email: req.body.email, password: req.body.password, coins: 0}
+        const hashed_password = await bcrypt.hash(req.body.password, 10);
+        const user_info = {username: req.body.username, email: req.body.email, password: hashed_password, coins: 0}
         username_query = {username: req.body.username}
         //TODO: figure out replacement for count()
         if (await users.find(username_query).count() > 0) {
@@ -39,16 +54,20 @@ apiRouter.post("/signup/", async (req, res) => {
 apiRouter.post("/login/", async (req, res) => {
     try {
         const users = db.collection("users");
-        const user_info = {username: req.body.username, email: req.body.email, password: req.body.password, coins: 0};
-        const login_query = {username: req.body.username, password: req.body.password};
+        const user_info = {username: req.body.username, email: req.body.email, coins: 0};
+        const login_query = {username: req.body.username};
         const user = await users.findOne(login_query);
         if (user == null) {
             res.send("Invalid Login");
             console.log("Invalid Login");
             return;
         }
+        if (await bcrypt.compare(req.body.password, user.password)) {
+            console.log("login success");
+            res.json(user_info)
+        }
         else {
-            res.json(user);      
+            res.send("Invalid Password");      
         }
 
     } catch(err) {
@@ -68,3 +87,5 @@ apiRouter.get("/sports/", async (req, res) => {
       res.sendStatus(500);
     }
   });
+
+  module.exports = apiRouter;
